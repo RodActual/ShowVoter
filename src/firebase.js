cae53d4f09b507eceae867c6984595ed29ee8cd1
Anthony Rodriguez <148160626+RodActual@users.noreply.github.com>
2025-12-11 22:14:24 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from ‘firebase/firestore’;
 
 // Replace this with your Firebase config from the Firebase Console
 const firebaseConfig = {
-apiKey: "AIzaSyCWbNluKAI5L2S3JoLHymzKFmNW01zMdVc",
-  authDomain: "watch-together-78712.firebaseapp.com",
-  projectId: "watch-together-78712",
-  storageBucket: "watch-together-78712.firebasestorage.app",
-  messagingSenderId: "645519961462",
-  appId: "1:645519961462:web:4b91bb2caffab357205e57",
-  measurementId: "G-5HXMP6VBF8"
+
 };
 
 // Initialize Firebase
