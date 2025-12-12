b110a5e0273c573f9e3a55429ff268087e84885b
Anthony Rodriguez <148160626+RodActual@users.noreply.github.com>
2025-12-11 20:09:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-initial build
+// src/App.js
+import React from ‘react’;
+import WatchTogether from ‘./WatchTogether’;
+import ‘./App.css’;
+
+function App() {
+return (
+<div className="App">
+<WatchTogether />
+</div>
+);
+}
+
+export default App;
